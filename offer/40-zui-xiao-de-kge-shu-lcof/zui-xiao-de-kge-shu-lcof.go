4de13aee3a6a754aa7a2079e12a7407a5445357d
lingongcheng <lingongcheng@zuoyebang.com>
2021-01-14 10:24:12 +0800

    40-zui-xiao-de-kge-shu-lcof

--- offer/40-zui-xiao-de-kge-shu-lcof/zui-xiao-de-kge-shu-lcof.go
+++ offer/40-zui-xiao-de-kge-shu-lcof/zui-xiao-de-kge-shu-lcof.go
@@ -1,1 +1,37 @@
 package _0_zui_xiao_de_kge_shu_lcof
+
+func GetLeastNumbers(arr []int, k int) []int {
+	al := len(arr)
+	if al <= k {
+		return arr
+	}
+
+	return quickSelect(arr, 0, al-1, k)
+}
+
+func quickSelect(arr []int, l, r, k int) []int {
+	i := partition(arr, l, r)
+	if i == k {
+		return arr[:k]
+	} else if i < k {
+		return quickSelect(arr, i+1, r, k)
+	} else {
+		return quickSelect(arr, l, i-1, k)
+	}
+}
+
+func partition(arr []int, l, r int) int {
+	pivot := arr[r]
+
+	i := l
+	for j := l; j < r; j++ {
+		if arr[j] <= pivot {
+			arr[i], arr[j] = arr[j], arr[i]
+			i++
+		}
+	}
+
+	arr[i], arr[r] = arr[r], arr[i]
+
+	return i
+}
--- offer/40-zui-xiao-de-kge-shu-lcof/zui-xiao-de-kge-shu-lcof_test.go
+++ offer/40-zui-xiao-de-kge-shu-lcof/zui-xiao-de-kge-shu-lcof_test.go
@@ -1,1 +1,20 @@
 package _0_zui_xiao_de_kge_shu_lcof
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGetLeastNumbers(t *testing.T) {
+	arr := []int{3,2,1}
+	k := 2
+	fmt.Println(GetLeastNumbers(arr, k))
+
+	arr = []int{0,1,2,1}
+	k = 1
+	fmt.Println(GetLeastNumbers(arr, k))
+
+	arr = []int{0,0,2,3,2,1,1,2,0,4}
+	k = 10
+	fmt.Println(GetLeastNumbers(arr, k))
+}
