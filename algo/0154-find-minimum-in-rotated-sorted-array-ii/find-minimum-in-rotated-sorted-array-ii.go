913ad687f2fe683497f12c09dceb98e75ba685fa
lingongcheng <lingongcheng@zuoyebang.com>
2021-01-17 19:06:31 +0800

    find-minimum-in-rotated-sorted-array-ii

--- algo/0154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.go
+++ algo/0154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.go
@@ -1,1 +1,53 @@
 package _154_find_minimum_in_rotated_sorted_array_ii
+
+//右指针
+func FindMin(numbers []int) int {
+	left := 0
+	right := len(numbers) - 1
+
+	//本身有序，直接返回
+	if numbers[left] < numbers[right] {
+		return numbers[left]
+	}
+
+	for left < right { //循环退出条件，left=right
+		mid := left + (right-left)>>1
+		if numbers[mid] < numbers[right] { //中间元素小于最后元素，那么右侧单调有序
+			right = mid //之所以是mid不是mid-1，有可能mid是最小元素
+		} else if numbers[mid] > numbers[right] { //最右元素小于中间元素，那么左侧单调有序
+			left = mid + 1
+		} else {
+			right--
+		}
+	}
+
+	return numbers[left]
+}
+
+//左指针
+func FindMin2(nums []int) int {
+	left := 0
+	right := len(nums) - 1
+
+	//本身有序，直接返回
+	if nums[left] < nums[right] {
+		return nums[left]
+	}
+
+	for left < right { //循环退出条件，left=right
+		if left >0 && nums[left] < nums[left - 1] {
+			break
+		}
+
+		mid := left + (right-left)>>1
+		if nums[left] < nums[mid] { //最左元素小于mid元素，那么左侧单调有序
+			left = mid + 1
+		} else if nums[mid] < nums[left] { //最左元素大于中间元素，那么右侧单调有序
+			right = mid 	//之所以是mid不是mid-1，有可能mid是最小元素
+		} else {
+			left++
+		}
+	}
+
+	return nums[left]
+}
--- algo/0154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii_test.go
+++ algo/0154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii_test.go
@@ -1,1 +1,26 @@
 package _154_find_minimum_in_rotated_sorted_array_ii
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFindMin(t *testing.T) {
+	numbers := []int{3, 4, 5, 1, 2}
+	fmt.Println("FindMin", numbers, FindMin(numbers))
+
+	numbers = []int{2, 2, 2, 0, 1}
+	fmt.Println("FindMin", numbers, FindMin(numbers))
+
+	numbers = []int{2, 2, 2, 0, 1, 2}
+	fmt.Println("FindMin", numbers, FindMin(numbers))
+
+	numbers = []int{3, 4, 5, 1, 2}
+	fmt.Println("FindMin2", numbers, FindMin2(numbers))
+
+	numbers = []int{2, 2, 2, 0, 1}
+	fmt.Println("FindMin2", numbers, FindMin2(numbers))
+
+	numbers = []int{2, 2, 2, 0, 1, 2}
+	fmt.Println("FindMin2", numbers, FindMin2(numbers))
+}
