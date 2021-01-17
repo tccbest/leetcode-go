49faa6211fa0eebaa4e4513491b388912d92e99b
lingongcheng <lingongcheng@zuoyebang.com>
2021-01-17 16:30:08 +0800

    xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof

--- offer/11-xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof/xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof.go
+++ offer/11-xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof/xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof.go
@@ -1,7 +1,5 @@
 package _1_xuan_zhuan_shu_zu_de_zui_xiao_shu_zi_lcof
 
-import "fmt"
-
 func MinArray(numbers []int) int {
 	left := 0
 	right := len(numbers) - 1
@@ -11,11 +9,11 @@ func MinArray(numbers []int) int {
 		return numbers[left]
 	}
 
-	for left < right {	//循环退出条件，left=right
-		mid := left + (right - left) >> 1
-		if numbers[mid] < numbers[right] {	//最左元素小于mid元素，那么左侧单调有序
-			right = mid	//之所以是mid不是mid-1，有可能mid是最小元素
-		} else if numbers[mid] > numbers[right] {	//最右元素大于mid元素，那么右侧单调有序
+	for left < right { //循环退出条件，left=right
+		mid := left + (right-left)>>1
+		if numbers[mid] < numbers[right] { //最左元素小于mid元素，那么左侧单调有序
+			right = mid //之所以是mid不是mid-1，有可能mid是最小元素
+		} else if numbers[mid] > numbers[right] { //最右元素大于mid元素，那么右侧单调有序
 			left = mid + 1
 		} else {
 			right--
