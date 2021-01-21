485bdfc202aecf43b4b878aafdde96c4ade75989
lingongcheng <lingongcheng@zuoyebang.com>
2021-01-21 21:55:46 +0800

    find-minimum-in-rotated-sorted-array

--- algo/0153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.go
+++ algo/0153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.go
@@ -1,1 +1,21 @@
 package _153_find_minimum_in_rotated_sorted_array
+
+func FindMin(nums []int) int {
+	left := 0
+	right := len(nums) - 1
+
+	if right == 0 {
+		return nums[0]
+	}
+
+	for left < right {	//退出条件left=right
+		mid := left + (right - left) >> 1
+		if nums[mid] < nums[right] {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+
+	return nums[left]
+}
--- algo/0153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array_test.go
+++ algo/0153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array_test.go
@@ -1,1 +1,21 @@
 package _153_find_minimum_in_rotated_sorted_array
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFindMin(t *testing.T) {
+	nums := []int{4, 5, 6, 7, 0, 1, 2}
+	fmt.Println(nums, FindMin(nums))
+
+	nums = []int{3, 1, 2}
+	fmt.Println(nums, FindMin(nums))
+
+	nums = []int{1, 2}
+	fmt.Println(nums, FindMin(nums))
+
+	nums = []int{2}
+	fmt.Println(nums, FindMin(nums))
+
+}
