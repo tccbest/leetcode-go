683987f5aa96a22e113cc18c4cc8cecccb2c25bd
lingongcheng <lingongcheng@zuoyebang.com>
2021-02-26 09:49:16 +0800

    search-insert-position

--- algo/0035-search-insert-position/search-insert-position.go
+++ algo/0035-search-insert-position/search-insert-position.go
@@ -2,24 +2,28 @@ package _035_search_insert_position
 
 func SearchInsert(nums []int, target int) int {
 	l := len(nums)
-	if l == 0 {
+	if l == 0 || target < nums[0] {
 		return 0
+	}
+
+	if target > nums[l - 1] {
+		return l
 	}
 
 	left := 0
 	right := l - 1
-
-	for left < right {
+	for left <= right {
 		mid := left + (right-left)>>1
-
-		if target <= nums[mid] {
-			right = mid - 1
-		} else {
+		if target == nums[mid] {
+			return mid
+		} else if target > nums[mid] {
 			left = mid + 1
+		} else if target < nums[mid] {
+			right = mid - 1
 		}
 	}
 
-	if target <= nums[left] {
+	if target < nums[left] {
 		return left
 	}
 
