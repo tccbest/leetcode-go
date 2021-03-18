1960ed72794d92d74e0b7b19cd816f4a87bedaad
lingongcheng <lingongcheng@zuoyebang.com>
2021-03-18 19:47:17 +0800

    implement-strstr

--- algo/0028-implement-strstr/implement-strstr.go
+++ algo/0028-implement-strstr/implement-strstr.go
@@ -1,1 +1,19 @@
 package _028_implement_strstr
+
+func strStr(haystack string, needle string) int {
+	if needle == "" {
+		return 0
+	}
+
+	left, right := 0, len(needle)
+	for right <= len(haystack) {
+		if needle == haystack[left:right] {
+			return left
+		}
+
+		left++
+		right++
+	}
+
+	return -1
+}
