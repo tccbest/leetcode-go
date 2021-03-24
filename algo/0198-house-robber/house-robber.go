17d15a0a8e552927a0c82dee655e716e97268572
lingongcheng <lingongcheng@zuoyebang.com>
2021-03-24 20:58:14 +0800

    house-robber

---
+++ algo/0198-house-robber/house-robber.go
@@ -0,0 +1,30 @@
+package _198_house_robber
+
+//动态规划
+func rob(nums []int) int {
+	l := len(nums)
+	if l == 0 {
+		return 0
+	}
+
+	if l == 1 {
+		return nums[0]
+	}
+
+	dp := make([]int, l)
+	dp[0] = nums[0]
+	dp[1] = max(nums[0], nums[1])
+	for i := 2; i < l; i++ {
+		dp[i] = max(dp[i-2]+nums[i], dp[i-1])
+	}
+
+	return dp[l - 1]
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+
+	return y
+}
---
+++ algo/0198-house-robber/house-robber_test.go
@@ -0,0 +1,1 @@
+package _198_house_robber
