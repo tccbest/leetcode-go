736c15773269fbf177e2e91ad36367a4aaf97751
lingongcheng <lingongcheng@zuoyebang.com>
2021-03-24 21:06:11 +0800

    house-robber

--- algo/0198-house-robber/house-robber.go
+++ algo/0198-house-robber/house-robber.go
@@ -11,6 +11,25 @@ func rob(nums []int) int {
 		return nums[0]
 	}
 
+	first := nums[0]
+	second := max(nums[0], nums[1])
+	for i := 2; i < l; i++ {
+		first, second = second, max(first+nums[i], second)
+	}
+
+	return second
+}
+
+func rob2(nums []int) int {
+	l := len(nums)
+	if l == 0 {
+		return 0
+	}
+
+	if l == 1 {
+		return nums[0]
+	}
+
 	dp := make([]int, l)
 	dp[0] = nums[0]
 	dp[1] = max(nums[0], nums[1])
